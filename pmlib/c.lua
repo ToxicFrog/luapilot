fcb82c1d153d1c990e52a1fbf19cf9b05f918628
Ben Kelly <bk@ancilla.ca>
2009-10-19 23:50:37 -0400

    Reorganized source into src/ and include/ subdirectories. Installed Prime Mover build system.

---
+++ pmlib/c.lua
@@ -0,0 +1,39 @@
+CC = "gcc"
+CCFLAGS = { "-Wall", "-c", "-std=c99" }
+INCLUDES = EMPTY
+COMPILE = "%CC% %CCFLAGS% %INCLUDES% -o %out[1]% %in[1]%"
+
+LD = "%CC%"
+LDFLAGS = EMPTY
+LIBS = EMPTY
+LINK = "%LD% %LDFLAGS% -o %out% %in% %LIBS%"
+
+
+cfile = simple {
+    class = "cfile";
+    command = { "%COMPILE%" };
+    outputs = { "%U%-%I%.o" };
+}
+
+lib = simple {
+    class = "lib";
+    command = {
+        "ar rcu %out% %in%";
+        "ranlib %out%";
+    };
+    outputs = { "%U%-%I%.a" };
+}
+
+sharedlib = simple {
+    class = "sharedlib";
+    command = { "%LINK%" };
+    LDFLAGS = { PARENT, "-shared" };
+    outputs = { "%U%-%I%.so" };
+}
+
+program = simple {
+    class = "program";
+    command = { "%LINK%" };
+    outputs = { "%U%-%I%" };
+}
+
